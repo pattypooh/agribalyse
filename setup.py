168ef2afe8f6b7f096fb60c115ed9b6cf2696d2f
Patricia Escalera <patty.escalera@gmail.com>
2021-10-24 04:44:33 +0200

    Project initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project for jedha bootcamp.  Full Stack Data Scientist',
+    author='Anatole Reffet, Aura Moreno, Malika Berrehail, Palera',
+    license='MIT',
+)
