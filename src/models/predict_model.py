72b8e49b980c46de08013bae13864cb10baa2d97
Patricia Escalera <patty.escalera@gmail.com>
2021-11-24 07:19:38 +0100

    Fix prob cache of ingrediends (in backend)

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -32,15 +32,47 @@ def generate_transposed_statistics(df):
     #Concatenate minEF and maxEF
     return pd.concat([minEF_df, maxEF_df], axis=1)
 
-def format_ingredients_list(user_input, canonical_df, statistics_df)->pd.DataFrame:
+def format_ingredients_list(user_input, base_df, statistics_df)->pd.DataFrame:
+    '''
+        Takes the user input of ingredients list and statistics_df and fills in the values in base_df
+        
+        Parameters
+        ------------------
+        user_input: 
+            List like of str (correponding to ingredients)
+        base_df:
+            The base dataframe to fill in with 1s for every ingredient in user_input and the statistics(min,max) for the same ingredients 
+        statistics_df
+            The statistics of every ingredient
+
+        Returns
+        --------------------
+        None 
+
+    '''
     statistics_user_input = generate_transposed_statistics(statistics_df.set_index('Ingredients').loc[user_input,:].reset_index())
-    canonical_df.loc[:,statistics_user_input.columns] = statistics_user_input
-    canonical_df.loc[:,user_input] = 1
+    base_df.loc[:,statistics_user_input.columns] = statistics_user_input
+    base_df.loc[:,user_input] = 1
+
 
 
 def predict(ingredients):
-    #Before predicting, add extra features
-    format_ingredients_list(ingredients, canonical_df, statistics_df)
+    '''
+    Launch the model and predicts the score of the list of ingredients
+    Parameters
+    ---------------
+    ingredients
+        - List type of str representend a list of ingredients used in a dish recipe
+    Returns
+    ----------------
+    A float value representing the score EF of the list of ingredients   
+    '''
 
-    score = score_predictor.predict(canonical_df)
+
+    base_df = canonical_df.copy()
+
+    #Before predicting, add extra features
+    format_ingredients_list(ingredients, base_df, statistics_df)
+    score = score_predictor.predict(base_df)
+
     return score
